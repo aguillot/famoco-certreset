c481c2233567cba6bef1996cfac4fe6802b63c5e
Adam Guillot <adam.guillot@gmail.com>
2022-03-18 16:13:35 +0100

    adding pytest

---
+++ config/settings/test.py
@@ -0,0 +1,9 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-d-_f8h=8(9&6ow#1&w)kja6)hj%g691f-9wgj44))^hfw_r@sv"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
